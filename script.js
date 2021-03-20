cbffb8573a5557f2a1a5604ab646f7a287d4b9a7
Calvin Roberts <roberts.calvin85@gmail.com>
2021-03-20 14:16:01 +0000

    Change Blur On Input.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use-strict';
+const password = document.getElementById('password'),
+  background = document.getElementById('background');
+
+password.addEventListener('input', (e) => {
+  const input = e.target.value,
+    length = input.length,
+    blurValue = 20 - length * 2;
+  background.style.filter = `blur(${blurValue}px)`;
+});
